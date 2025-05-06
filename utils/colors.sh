4508c994d1dfb18a23b3ddc0a3f44ccfd4bb6525
Gabriele Cerqueglini <gabriele.cerqueglini@codebaker.it>
2025-05-06 16:28:27 +0200

    fix: colors and erros utils methods name

--- utils/colors.sh
+++ utils/colors.sh
@@ -8,28 +8,28 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Function to print colored messages
-utils::print_colored() {
+colors::print_colored() {
     local color=$1
     local message=$2
     echo -e "${color}${message}${NC}"
 }
 
 # Function to print success message
-utils::print_success() {
-    utils::print_colored "$GREEN" "$1"
+colors::print_success() {
+    colors::print_colored "$GREEN" "$1"
 }
 
 # Function to print error message
-utils::print_error() {
-    utils::print_colored "$RED" "$1"
+colors::print_error() {
+    colors::print_colored "$RED" "$1"
 }
 
 # Function to print warning message
-utils::print_warning() {
-    utils::print_colored "$YELLOW" "$1"
+colors::print_warning() {
+    colors::print_colored "$YELLOW" "$1"
 }
 
 # Function to print info message
-utils::print_info() {
-    utils::print_colored "$BLUE" "$1"
+colors::print_info() {
+    colors::print_colored "$BLUE" "$1"
 }
--- utils/error.sh
+++ utils/error.sh
@@ -10,19 +10,19 @@ error::handle_error() {
     local last_command=$4
     local func_trace=$5
 
-    print_error "Error occurred in script at line: $line_no"
-    print_error "Last command executed: $last_command"
-    print_error "Exit code: $exit_code"
-    print_error "Function trace: $func_trace"
-    print_error "Bash line number: $bash_lineno"
+    colors::print_error "Error occurred in script at line: $line_no"
+    colors::print_error "Last command executed: $last_command"
+    colors::print_error "Exit code: $exit_code"
+    colors::print_error "Function trace: $func_trace"
+    colors::print_error "Bash line number: $bash_lineno"
     
     # Cleanup on error
-    cleanup_on_error
+    error::cleanup_on_error
 }
 
 error::cleanup_on_error() {
     if [ -d "$TEMP_DIR" ]; then
         rm -rf "$TEMP_DIR"
     fi
-    print_info "Cleaned up temporary files"
+    colors::print_info "Cleaned up temporary files"
 }
